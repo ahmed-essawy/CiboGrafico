5205c0afd11eab3eb26863c044ae08cde83e5826
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-05-28 14:24:13 +0200

    Mob App First Settings

--- MobApp/src/app/app.component.ts
+++ MobApp/src/app/app.component.ts
@@ -1,22 +1,19 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
-
-import { HomePage } from '../pages/home/home';
-
-
+﻿import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar, Splashscreen } from "ionic-native";
+import { HomePage } from "../pages/home/home";
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage = HomePage;
-
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    rootPage = HomePage;
+    constructor(platform: Platform) {
+        platform.ready().then(() => {
+            if (!platform.is("cordova")) return;
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
+        });
+    }
 }
