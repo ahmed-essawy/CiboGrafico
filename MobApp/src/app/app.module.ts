dd7eda815f784ad0e202eac87670ceea23cdf1d0
Sohaila Tarek <sohailatarekkhalifa@gmail.com>
2017-06-03 14:37:55 +0200

    Mobile app add restaurants & reservations

--- MobApp/src/app/app.module.ts
+++ MobApp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 ﻿import { NgModule, ErrorHandler } from "@angular/core";
 import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { HTTP } from "@ionic-native/http";
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 import { IntroPage } from "../pages/intro/intro";
 import { MenuPage } from "../pages/menu/menu";
+import { RestaurantsListPage } from "../pages/RestaurantsList/RestaurantsList";
+import { RestaurantProfilePage } from "../pages/RestaurantProfile/RestaurantProfile";
+import {reservationPage} from "../pages/reservation/reservation"
+import { Ionic2RatingModule } from 'ionic2-rating';
 @NgModule({
-    declarations: [MyApp, HomePage, IntroPage, MenuPage],
-    imports: [IonicModule.forRoot(MyApp)],
+    declarations: [MyApp, HomePage, IntroPage, MenuPage, RestaurantsListPage, RestaurantProfilePage, reservationPage],
+    imports: [IonicModule.forRoot(MyApp), Ionic2RatingModule],
     bootstrap: [IonicApp],
-    entryComponents: [MyApp, HomePage, IntroPage, MenuPage],
-    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
+    entryComponents: [MyApp, HomePage, IntroPage, MenuPage, RestaurantsListPage, RestaurantProfilePage, reservationPage],
+    providers: [HTTP, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule { }
---
+++ MobApp/src/pages/RestaurantProfile/RestaurantProfile.ts
@@ -0,0 +1,26 @@
+﻿import { Component, Input} from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { HTTP } from "@ionic-native/http";
+import { reservationPage } from "../reservation/reservation"
+@Component({
+    selector: 'page-RestaurantProfile',
+    templateUrl: 'RestaurantProfile.html'
+})
+export class RestaurantProfilePage {
+    restaurant:any;
+    constructor(private http: HTTP,public navCtrl: NavController, public params: NavParams) {
+        let id = params.get("Id");
+        this.http.get("http://169.254.80.80:8888/Restaurants/" + id, {}, {})
+            .then(data => {
+                this.restaurant = JSON.parse(data.data).data;
+            })
+            .catch(error => {
+                console.log("error");
+                console.log(error);
+            });
+    }
+    reserve(restId) {
+        this.navCtrl.push(reservationPage, { Id: restId});
+    }
+
+}
