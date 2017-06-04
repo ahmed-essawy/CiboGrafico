0cea4920b2815808b494814df523d1d12645edae
omarhamdyrawash <omarhamdy09@yahoo.com>
2017-06-04 18:29:10 +0200

    Add readbyuser to Orders

--- Server/Scripts/Sources/Access Layers/Orders.ts
+++ Server/Scripts/Sources/Access Layers/Orders.ts
@@ -1,13 +1,14 @@
-﻿import { Order, SubOrder } from "../Classes";
+﻿import { Order, SubOrder, Restaurant } from "../Classes";
 import { objectId, Id, Order_Rate } from "../Types";
 import { Collection } from "../Mongodb";
 module.exports = {
         Collection: () => Collection("Orders"),
         Read(object: Id, callback: any) {
-            Collection("Orders").findOne(objectId(object._id), (err, row: Order) => {
-                if (row) return callback({ success: true, data: row });
-                else return callback({ success: false, data: object });
-            });
+            Collection("Orders").findOne(objectId(object._id),
+                (err, row: Order) => {
+                    if (row) return callback({ success: true, data: row });
+                    else return callback({ success: false, data: object });
+                });
         },
         ReadAll(callback: any) {
             Collection("Orders").find().toArray((err, row: Order[]) => {
@@ -16,18 +17,21 @@ module.exports = {
             });
         },
         Delete(object: Id, callback: any) {
-            Collection("Orders").removeOne({ "_id": objectId(object._id) }, (err, resp) => {
-                if (resp.result.ok) {
-                    Collection("Users").update({ "_id": object._id },
-                        { $pull: { "orders": object._id } });
-                    return callback({ success: true, data: resp.result });
-                } else return callback({ success: false, data: object });
-            });
+            Collection("Orders").removeOne({ "_id": objectId(object._id) },
+                (err, resp) => {
+                    if (resp.result.ok) {
+                        Collection("Users").update({ "_id": object._id },
+                            { $pull: { "orders": object._id } });
+                        return callback({ success: true, data: resp.result });
+                    } else return callback({ success: false, data: object });
+                });
         },
         Create(object: Order, callback: any) {
             object.owner = objectId(object.owner);
             object.restaurant = objectId(object.restaurant);
-            Collection("Orders").update({ "num": object.num }, { $setOnInsert: object }, { upsert: true },
+            Collection("Orders").update({ "num": object.num },
+                { $setOnInsert: object },
+                { upsert: true },
                 (err, resp) => {
                     if (resp.result.upserted) {
                         Collection("Users").update({ "_id": object.owner },
@@ -39,14 +43,17 @@ module.exports = {
         },
         Update(object: Order, callback: any) {
             object._id = objectId(object._id);
-            Collection("Orders").updateOne({ _id: object._id }, { $set: object }, (err, row: Order) => {
-                if (row) return callback({ success: true, data: row });
-                else return callback({ success: false });
-            });
+            Collection("Orders").updateOne({ _id: object._id },
+                { $set: object },
+                (err, row: Order) => {
+                    if (row) return callback({ success: true, data: row });
+                    else return callback({ success: false });
+                });
         },
         RateOrder(object: Order_Rate, callback: any) {
             object._id = objectId(object._id);
-            Collection("Orders").updateOne({ _id: object._id }, { $set: { rate: object.rate } },
+            Collection("Orders").updateOne({ _id: object._id },
+                { $set: { rate: object.rate } },
                 (err, row: Order) => {
                     if (row) return callback({ success: true, data: row });
                     else return callback({ success: false });
@@ -70,5 +77,27 @@ module.exports = {
                     if (row) return callback({ success: true, data: row });
                     return callback({ success: false });
                 });
+        },
+        ReadAllByUser(object: Id, callback: any) {
+            Collection("Orders").find({ "owner": objectId(object._id) }).toArray((err, row: Order[]) => {
+                if (err) return callback({ success: false, msg: "Error !!" });
+                if (row) {
+                    const restaurants = async function () {
+                        const orders = Array<Order>();
+                        for (let i = 0; i < row.length; i++) {
+                            await new Promise(resolve => {
+                                Collection("Restaurants").findOne(objectId(row[i].restaurant),
+                                    (err, datarow: Restaurant) => {
+                                        if (err) return callback({ success: false, msg: "Error !!" });
+                                        row[i]["restName"] = datarow.name;
+                                        resolve(row[i]);
+                                    });
+                            }).then(order => orders.push(order as Order));
+                        }
+                        return orders;
+                    };
+                    restaurants().then(orders => callback({ success: true, data: orders }));
+                } else return callback({ success: false });
+            });
         }
     };
--- Server/Scripts/Sources/Routes/Orders.ts
+++ Server/Scripts/Sources/Routes/Orders.ts
@@ -5,34 +5,52 @@ import { Order_Rate, objectId, OrderType } from "../Types";
     orders
         .get("/", (req: any, res: any) => db.Orders.ReadAll(response => res.json(response)))
         .get("/:id", (req, res) => db.Orders.Read({ _id: req.params.id }, response => res.json(response)))
-        .get("/SubOrders/:num", (req, res) => db.Orders.GetSubOrders(parseInt(req.params.num), response => res
-            .json(response)))
+        .get("/UserOrder/:id",
+        (req, res) => db.Orders.ReadAllByUser({ _id: req.params.id }, response => res.json(response)))
+        .get("/SubOrders/:num",
+        (req, res) => db.Orders.GetSubOrders(parseInt(req.params.num),
+            response => res
+                .json(response)))
         .delete("/:id", (req, res) => db.Orders.Delete({ _id: req.params.id }, response => res.json(response)))
-        .post("/", (req, res) => {
-            if (req.body.num && req.body.owner && req.body.restaurant && req.body.type && Array.isArray(req.body
-                .meals)) {
+        .post("/",
+        (req, res) => {
+            if (req.body.num &&
+                req.body.owner &&
+                req.body.restaurant &&
+                req.body.type &&
+                Array.isArray(req.body
+                    .meals)) {
                 let tempAddress = new Address();
                 if (req.body.address)
-                    tempAddress = new Address(req.body.address.street, req.body.address.city, req.body.address.country);
-                const tempOrder = new Order(db.objectId(), req.body.num, req.body.owner, req.body.restaurant, req.body
-                    .type, tempAddress, req.body.meals);
+                    tempAddress = new Address(req.body.address.street,
+                        req.body.address.city,
+                        req.body.address.country);
+                const tempOrder = new Order(db.objectId(),
+                    req.body.num,
+                    req.body.owner,
+                    req.body.restaurant,
+                    req.body
+                        .type,
+                    tempAddress,
+                    req.body.meals);
                 db.Orders.Create(tempOrder, response => res.json(response));
             } else res.status(400).json({ success: false, msg: "Invalid Data" });
         })
         .post("/CreateSubOrder",
-            (req, res) => {
-                if (req.body.num && req.body.owner) {
-                    const tempSubOrder = new SubOrder(db.objectId(), req.body.num, req.body.owner);
-                    db.Orders.Create(tempSubOrder, response => res.json(response));
-                    if (Array.isArray(req.body.meals)) tempSubOrder.meals = req.body.meals;
-                } else res.status(400).json({ success: false, msg: "Invalid Data" });
-            })
-        .put("/", (req, res) => {
+        (req, res) => {
+            if (req.body.num && req.body.owner) {
+                const tempSubOrder = new SubOrder(db.objectId(), req.body.num, req.body.owner);
+                db.Orders.Create(tempSubOrder, response => res.json(response));
+                if (Array.isArray(req.body.meals)) tempSubOrder.meals = req.body.meals;
+            } else res.status(400).json({ success: false, msg: "Invalid Data" });
+        })
+        .put("/",
+        (req, res) => {
             if (req.body._id) {
                 db.Orders.Read({ _id: req.body._id },
                     response => {
                         if (response.success) {
-                            let tempOrder: Order = response.data;
+                            const tempOrder: Order = response.data;
                             if (req.body.type) tempOrder.type = OrderType[parseInt(req.body.type)];
                             if (req.body.address) {
                                 if (req.body.address.street) tempOrder.address.street = req.body.address.street;
@@ -45,7 +63,8 @@ import { Order_Rate, objectId, OrderType } from "../Types";
                     });
             } else res.status(400).json({ success: false, msg: "Invalid Data" });
         })
-        .put("/RateOrder", (req, res) => {
+        .put("/RateOrder",
+        (req, res) => {
             if (req.body._id) {
                 db.Orders.Read({ _id: req.body._id },
                     response => {
