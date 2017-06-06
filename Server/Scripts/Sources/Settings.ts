29963e15cfc41b54505f1f61f1b0e775d1defb48
omarhamdyrawash <omarhamdy09@yahoo.com>
2017-06-06 22:33:33 +0200

    Fix Put Request errpr

--- Server/Scripts/Sources/Settings.ts
+++ Server/Scripts/Sources/Settings.ts
@@ -3,9 +3,10 @@
     res.setHeader("X-Powered-By", "Project");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header("Access-Control-Allow-Methodss", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Method", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    //if (req.method === "OPTIONS") res.sendStatus(200);
+    if (req.method === "OPTIONS") res.sendStatus(200);
     //else if (["PUT", "POST", "DELETE"].indexOf(req.method) > -1) {
     //    require("./Mongodb").Users.Read({ "login.token": req.query.access_token },
     //        users => {
