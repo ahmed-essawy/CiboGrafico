60277bd78b79702dbde95c809e439153c8a5d16f
Sohaila Tarek <sohailatarekkhalifa@gmail.com>
2017-06-07 22:38:00 +0200

    Change in reservation pages

--- MobApp/src/pages/RestaurantProfile/RestaurantProfile.ts
+++ MobApp/src/pages/RestaurantProfile/RestaurantProfile.ts
@@ -2,15 +2,23 @@
 import { NavController, NavParams } from 'ionic-angular';
 import { Restaurants, PromiseResp } from "../../providers/restaurants";
 import { reservationPage } from "../reservation/reservation"
-import {TraditionalMenuPage } from "../traditionalMenu/traditionalMenu"
+import { TraditionalMenuPage } from "../traditionalMenu/traditionalMenu"
+import { Users } from "../../providers/users";
+import { Sql } from "../../providers/sql";
 @Component({
     selector: 'page-RestaurantProfile',
     templateUrl: 'RestaurantProfile.html'
 })
 export class RestaurantProfilePage {
-    restaurant:any;
+    restaurant: any;
+    isLogged: boolean;
+    userId: any;
+    comment:any;
+    date:any=new Date().toISOString();
     constructor(public navCtrl: NavController, public params: NavParams, private rest: Restaurants) {
-        this.rest.read(params.get("Id")).then((resp: PromiseResp) => this.restaurant = resp.response).catch(err => console.log(err));
+        this.rest.read(params.get("Id")).then((resp: PromiseResp) => console.log(this.restaurant = resp.response)).catch(err => console.log(err));
+        Users.isLogged().then(isLogged => this.isLogged = isLogged);
+        Sql.selectOptions("_id").then(resp => this.userId = resp.response).catch(err => console.log(err));
     }
     reserve(restId) {
         this.navCtrl.push(reservationPage, { Id: restId});
@@ -18,4 +26,12 @@ export class RestaurantProfilePage {
     menu(restId) {
         this.navCtrl.push(TraditionalMenuPage, { Id: restId });
     }
+    submit(data) {
+        data.value._id = this.userId;
+        console.log(data.value);
+        this.rest.addReview({"restaurant": this.params.get("Id"),"review": data.value})
+            .then((resp: PromiseResp) => console.log(resp)).catch(err => console.log(err));
+        this.comment = "";
+    }
+
 }
--- MobApp/src/pages/reservation/reservation.ts
+++ MobApp/src/pages/reservation/reservation.ts
@@ -1,17 +1,48 @@
 ﻿import { Component } from '@angular/core';
 import { Restaurants, PromiseResp } from "../../providers/restaurants";
-import { NavController, NavParams } from 'ionic-angular';
+import { Sql} from "../../providers/sql";
+import { NavController, NavParams, ToastController } from 'ionic-angular';
+import {RestaurantsListPage } from "../RestaurantsList/RestaurantsList"
 @Component({
     selector: 'page-reservation',
     templateUrl: 'reservation.html'
 })
 export class reservationPage {
-    branches:any;
-    constructor(public navCtrl: NavController, public navParams: NavParams, private restaurant: Restaurants) {
-        console.log(navParams.get("Id"));
-        this.restaurant.readBranches(navParams.get("Id")).then((resp: PromiseResp) => this.branches = resp.response).catch(err => console.log(err));
-
+    branches: any;
+    userId: any;
+    owner: any;
+    date: any;
+    guests: any;
+    time: any;
+    branch:any;
+    constructor(public navCtrl: NavController, public navParams: NavParams, private restaurant: Restaurants,
+        public toastCtrl: ToastController) {
+        this.restaurant.readBranches(navParams.get("Id")).then((resp: PromiseResp) => this.branches = resp.response)
+            .catch(err => console.log(err));
+        Sql.selectOptions("_id").then(resp => console.log(this.userId = resp.response)).catch(err => console.log(err));
+        Sql.selectOptions("firstName")
+            .then(resp => Sql.selectOptions("lastName")
+                .then(res => this.owner = resp.response + " " + res.response)
+                .catch(error => console.log(error)))
+            .catch(err => console.log(err));
+        this.date = new Date().toISOString();
+        this.time = new Date(new Date().getTime() + 3 * 3600000).toISOString();
+        this.guests = 2;
     }
-
+    reserve(data) {
+        data.value.owner = this.userId;
+        console.log(data.value);
+        this.restaurant.addReservation({"restaurant": this.navParams.get("Id"),"reservation": data.value})
+            .then((resp: PromiseResp) => console.log(resp)).catch(err => console.log(err));
+        let toast = this.toastCtrl.create({
+            message: 'You Reserved Successfully',
+            showCloseButton: true,
+            closeButtonText: "Ok"
+    });
+        toast.present();  
+        toast.onDidDismiss(() => {
+            this.navCtrl.push(RestaurantsListPage);
+        });
+    }
 
 }
