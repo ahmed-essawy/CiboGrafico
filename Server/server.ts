da75f60b75417e7101414f62b31ae1984cc6a3c4
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-06-12 15:20:24 +0200

    Change running path for azure

--- Server/server.ts
+++ Server/server.ts
@@ -6,7 +6,7 @@ server.use(require("./Scripts/Sources/Settings").settings)
 require("./Scripts/Sources/Mongodb").connectToServer(err => {
     if (err) console.log("Failed to connect to database!");
     else {
-        const port = process.env.port || 8888;
+        const port = process.env.port || 1337;
         require("http").createServer(server).listen(port, function () { console.log("Server listening at %s", this.address().port) });
     }
 });
